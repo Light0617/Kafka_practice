313e529e4d346334d27e7419f30e03de7feff6f2
light0617 <light11470617@gmail.com>
2018-10-16 21:38:58 -0700

    complete testing

--- src/main/java/com/github/WordCountApp.java
+++ src/main/java/com/github/WordCountApp.java
@@ -1,17 +1,36 @@
 package com.github;
 
+import java.util.Properties;
+import java.util.Arrays;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KStreamBuilder;
 import org.apache.kafka.streams.kstream.KTable;
-
-import java.util.Arrays;
-import java.util.Properties;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
 
 public class WordCountApp {
+
+    public Topology createTopology(){
+        StreamsBuilder builder = new StreamsBuilder();
+
+        KStream<String, String> textLines = builder.stream("word-count-input");
+        KTable<String, Long> wordCounts = textLines
+                .mapValues(textLine -> textLine.toLowerCase())
+                .flatMapValues(textLine -> Arrays.asList(textLine.split("\\W+")))
+                .selectKey((key, val) -> val)
+                .groupByKey()
+                .count(Materialized.as("Counts"));
+        wordCounts.toStream().to("word-count-output", Produced.with(Serdes.String(), Serdes.Long()));
+
+        return builder.build();
+    }
+
     public static void main(String[] args) {
         Properties config = new Properties();
         config.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-application");
@@ -20,31 +39,17 @@ public class WordCountApp {
         config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
-        KStreamBuilder builder = new KStreamBuilder();
-        KStream<String, String> textLines = builder.stream("word-count-input");
-        KTable<String, Long> wordCounts = textLines
-                .mapValues(textLine -> textLine.toLowerCase())
-                .flatMapValues(textLine -> Arrays.asList(textLine.split("\\W+")))
-                .selectKey((key, word) -> word)
-                .groupByKey()
-                .count();
-
-        wordCounts.to(Serdes.String(), Serdes.Long(), "word-count-output");
-
-        KafkaStreams streams = new KafkaStreams(builder, config);
+        WordCountApp wordCountApp = new WordCountApp();
+        KafkaStreams streams = new KafkaStreams(wordCountApp.createTopology(), config);
         streams.start();
 
         // shutdown hook to correctly close the streams application
         Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
 
-
-        //System.out.println(streams.toString());
-        // Update:
-        // print the topology every 30 seconds for learning purposes
         while(true){
-            System.out.println(streams.toString());
+            streams.localThreadsMetadata().forEach(data -> System.out.println(data));
             try {
-                Thread.sleep(30000);
+                Thread.sleep(5000);
             } catch (InterruptedException e) {
                 break;
             }
